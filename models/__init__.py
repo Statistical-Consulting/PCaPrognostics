7e714a665c4cbe4dff9cb54de29dddd415ca3d28
Laetitia Frost <51784216+laefrost@users.noreply.github.com>
2024-11-14 23:33:12 +0100

    modified init

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from .base_model import BaseSurvivalModel
-from .rsf_model import RSFModel
-from .deep_surv_model import DeepSurvModel
-__all__ = ['BaseSurvivalModel', 'RSFModel', 'DeepSurvModel']
+#from .base_model import BaseSurvivalModel
+#from .rsf_model import RSFModel
+#from .deep_surv_model import DeepSurvModel
+#__all__ = ['BaseSurvivalModel', 'RSFModel', 'DeepSurvModel']
