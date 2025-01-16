a4db088fb97013c5b89746c9b1bdd543762c20db
Jonas Schernich <Jonas.schernich@protonmail.com>
2025-01-16 10:23:24 +0100

    fixed bug

--- preprocessing/preprocessing_2.R
+++ preprocessing/preprocessing_2.R
@@ -1,10 +1,13 @@
-pData <- as.data.frame(read_csv('data/merged_data/pData/imputed/test_pData_imputed.csv', lazy = TRUE))
+pData <- as.data.frame(read_csv('data/merged_data/pData/original/test_pData.csv', lazy = TRUE))
 common_genes <- read.csv(file.path(".", "data", "merged_data", "exprs", "common_genes", "common_genes_test_imputed.csv"), row.names=1)
 intersect_genes <- read.csv(file.path(".", "data", "merged_data", "exprs", "intersection", "intersect_test_genes_imputed.csv"), row.names=1)
 all_genes <- read.csv(file.path(".", "data", "merged_data", "exprs", "all_genes", "all_genes_test.csv"), row.names=1)
+scores <- read.csv(file.path(".", "data", "scores", "test_scores.csv"), row.names=1)
+
+
 # Split pData
-pData_cohort1 <- pData[grep("test_cohort_1", rownames(pData)), ]
-pData_cohort2 <- pData[grep("test_cohort_2", rownames(pData)), ]
+pData_cohort1 <- pData[grep("test_cohort_1", pData$...1), ]
+pData_cohort2 <- pData[grep("test_cohort_2", pData$...1), ]
 
 # Split common_genes
 common_genes_cohort1 <- common_genes[grep("test_cohort_1", rownames(common_genes)), ]
@@ -17,6 +20,11 @@ intersect_genes_cohort2 <- intersect_genes[grep("test_cohort_2", rownames(inters
 # Split all_genes
 all_genes_cohort1 <- all_genes[grep("test_cohort_1", rownames(all_genes)), ]
 all_genes_cohort2 <- all_genes[grep("test_cohort_2", rownames(all_genes)), ]
+
+rownames(scores) <- pData[,1]
+scores_cohort1 <- scores[grep("test_cohort_1", rownames(scores)), ]
+scores_cohort2 <- scores[grep("test_cohort_2", rownames(scores)), ]
+
 
 # Save pData splits
 write.csv(pData_cohort1, file.path(".", "data", "cohort_data", "pData", "imputed", "test_pData_cohort1_imputed.csv"))
@@ -33,3 +41,11 @@ write.csv(intersect_genes_cohort2, file.path(".", "data", "cohort_data", "exprs"
 # Save all_genes splits
 write.csv(all_genes_cohort1, file.path(".", "data", "cohort_data", "exprs", "all_genes_test_cohort1.csv"))
 write.csv(all_genes_cohort2, file.path(".", "data", "cohort_data", "exprs", "all_genes_test_cohort2.csv"))
+
+# Save score splits
+write.csv(scores_cohort1, file.path(".", "data", "scores", "test_scores_cohort1.csv"))
+write.csv(scores_cohort2, file.path(".", "data", "scores", "test_scores_cohort2.csv"))
+
+
+
+
