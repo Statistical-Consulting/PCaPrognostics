c4190abadcac94e205610043069f9a29bfac568f
Laetitia Frost <51784216+laefrost@users.noreply.github.com>
2025-02-09 12:08:01 +0100

    cleaned up rsf

--- utils/analysis.py
+++ utils/analysis.py
@@ -5,30 +5,48 @@ import re
 
 
 def extract_values(row):
+    """
+    Extracts 'test_cohort' and 'test_score' values from a given row string.
+    Args:
+        row (str): A string containing key-value pairs in a structured format.
+    
+    Returns:
+        tuple: (test_cohort, test_score) where:
+            - test_cohort (str or None): Extracted test cohort name.
+            - test_score (float or None): Extracted test score, converted to float.
+    """
     if not isinstance(row, str):
-        return None, None  # Handle non-string cases
+        return None, None 
     
-    # Regex patterns
     test_cohort_match = re.search(r"'test_cohort':\s*'([^']+)'", row)
-    test_score_match = re.search(r"'test_score':\s*([\d\.]+)", row)  # Numeric value
+    test_score_match = re.search(r"'test_score':\s*([\d\.]+)", row)  
     
-    # Extract matched values
     test_cohort = test_cohort_match.group(1) if test_cohort_match else None
     test_score = float(test_score_match.group(1)) if test_score_match else None
     if test_score is None: 
         test_score_match = re.search(r"'test_score':\s*array\(([\d.]+)", row)
         test_score = float(test_score_match.group(1)) if test_score_match else None
-
-    
     return test_cohort, test_score
 
-def load_split_results(results_path): 
+
+def load_split_results(results_path, model_name = None):
+    """
+    Loads and processes cross-validation result files from a directory.
+ 
+    Args:
+        results_path (str): Path to the directory containing CSV files.
+        model_name (str): Name of model class
+    
+    Returns:
+        pd.DataFrame: A combined DataFrame containing processed results
+    """
     csv_files = [os.path.join(results_path, file) for file in os.listdir(results_path) if file.endswith('.csv')]
     
     combined_data = []
     for file in csv_files: 
         df = pd.read_csv(file, index_col=0)
-        df['model'] =os.path.basename(file).replace("_cv.csv", "")
+        df['model_class'] = model_name
+        df['model'] = os.path.basename(file).replace(".csv", "")
         df[['test_cohort', 'ci']] = df['fold_results'].apply(lambda x: pd.Series(extract_values(x)))
                 
         contains_pData = bool(re.search(r"pData", file, re.IGNORECASE))
@@ -45,41 +63,48 @@ def load_split_results(results_path):
             "Scores" if contains_scores else ""
         ]
 
-        # Join non-empty components into a single string with a separator (e.g., "_")
         dataset = "_".join(filter(None, components)) 
         df['dataset'] = dataset   
-        
-        df.drop(['fold_results', 'mean_score', 'std_score'], axis=1, inplace=True)
-         
+        df.drop(['fold_results', 'mean_score', 'std_score'], axis=1, inplace=True) 
         combined_data.append(df)
     
     df_final = pd.concat(combined_data)
     return df_final
 
+
 def load_all_results(results_path): 
-    # Get a list of all CSV files in the "results" folder
+    """
+    Loads and aggregates all cross-validation result files from a directory.
+    
+    Args:
+        results_path (str): Path to the directory containing CSV files.
+    
+    Returns:
+        pd.DataFrame: Aggregated DataFrame
+    """
     csv_files = [os.path.join(results_path, file) for file in os.listdir(results_path) if file.endswith('.csv')]
-    # Read all CSV files, add a "name" column, and combine them into one dataframe
     combined_data = pd.concat(
         [
-            # Read each CSV file and add a "name" column with the file name
-            pd.read_csv(file).assign(model=os.path.basename(file).replace("_cv.csv", "")) for file in csv_files
+            pd.read_csv(file).assign(model=os.path.basename(file).replace(".csv", "")) for file in csv_files
         ],
-        ignore_index=True  # Reset the index in the combined dataframe
+        ignore_index=True 
     )
     combined_data = combined_data.loc[:, ['model', 'mean_score' ,'std_score']]
     combined_data = combined_data.groupby('model', as_index=False).agg(mean=('mean_score', 'mean'), sd = ('std_score', 'mean'))
-    # View the combined data
     return combined_data
 
-# Not necessary due to different sd structure
-# def aggregate_results(results):
-#     results_aggr = results.groupby('model', as_index=False).agg(mean=('ci', 'mean'), sd=('ci', 'std'))
-#     return results_aggr
 
+def combine_results(results_nstd, results_test = None):
+    """
+    Merges nested resampling results with test results.
 
-# TODO: Ergbebnisse aus Test und Nested reampling kombiniernen
-def combine_results(results_nstd, results_test):
+    Args:
+        results_nstd (pd.DataFrame): DataFrame containing nested resampling results.
+        results_test (pd.DataFrame or None): DataFrame containing test results or None.
+
+    Returns:
+        pd.DataFrame: merged DataFrame
+    """
     if results_test is not None: 
         combined_results = results_nstd.merge(results_test, how = "left", left_on = 'model', right_on = 'model')
     else: 
@@ -89,15 +114,17 @@ def combine_results(results_nstd, results_test):
     return(combined_results)
 
 
-# TODO: Dataframe erstellen: Spalte 1: Name des Feautres, Spalte 2: Wert
-# -------------------- functions to load feat. imp from model
-def load_feat_imp(model_path):
-    with open(model_path, 'rb') as file:
-        model = pickle.load(file)
+def load_feat_imp(model):
+    """
+    Loads and processes feature importance values from a saved model.
+    Args:
+        model : saved model object.
     
-    # Cat boost specific
-    #print(model)
-    # bei den Modellen die keine eigene Modellklasse von uns haben, muss man gucken wie der library interne Aufruf ist
+    Returns:
+        pd.DataFrame: A DataFrame containing:
+            - 'feature': Name of the feature.
+            - 'value': Importance score of the feature.
+    """
     imps = model.model.get_feature_importance()
     
     df = pd.DataFrame({
@@ -107,51 +134,120 @@ def load_feat_imp(model_path):
     
     df = df.sort_values(by = "value", ascending=False)
     df = df[df.loc[: , 'value'] > 0]
-    
     return df
 
-def load_model(model_path): 
-    with open(model_path, 'rb') as file:
-        model = pickle.load(file)
-    
-    return model
 
-def extract_scores_from_files(folder_path):
+def feat_imp_all_models(model_path, model_name, DATA_CONFIG): 
     """
-    Reads all files in a given folder, extracts the 'mean_score' and 'std_score' 
-    columns from the first row of each file, and saves them into a new DataFrame.
-
+    Computes and aggregates feature importance values for all models in a given directory.
     Args:
-        folder_path (str): Path to the folder containing the files.
+        model_path (str): Path to the directory containing trained model files (.pkl).
+        model_name (str): Name of the model type (e.g., CatBoost, XGBoost, etc.).
+        DATA_CONFIG (dict): Dictionary containing data configuration settings.
 
     Returns:
-        pd.DataFrame: A DataFrame with columns ['Model', 'mean_score', 'std_score'].
+        pd.DataFrame: A DataFrame containing feature importances
     """
-    # Initialize an empty list to store the extracted data
-    data = []
+    files = os.listdir(model_path)
+    imps_list = []
+    
+    for file in files:
+        contains_pData = bool(re.search(r"pData", file, re.IGNORECASE))
+        contains_intersection = bool(re.search(r"inter|int|intersection", file, re.IGNORECASE))
+        contains_imputed = bool(re.search(r"imp|imputed|common", file, re.IGNORECASE))
+        contains_aenc = bool(re.search(r"aenc|auto|autoenc|autoencoder", file, re.IGNORECASE))
+        contains_scores = bool(re.search(r"score|scores", file, re.IGNORECASE))
+        
+        # Load data based on file type
+        if contains_intersection:
+            DATA_CONFIG['gene_type'] = 'intersection'
+        elif contains_imputed:
+            DATA_CONFIG['gene_type'] = 'common_genes'
+        elif contains_aenc:
+            DATA_CONFIG['gene_type'] = 'intersection'
+        elif contains_scores: 
+            DATA_CONFIG['gene_type'] = 'scores'
+        if contains_pData:
+            DATA_CONFIG['clinical_covs'] = ["AGE", "TISSUE", "GLEASON_SCORE", 'PRE_OPERATIVE_PSA']
+        if contains_pData and not contains_intersection and not contains_imputed and not contains_aenc and not contains_scores: 
+            DATA_CONFIG['only_pData'] = True
+            DATA_CONFIG['gene_type'] = None
+            
+        model = load_model(os.path.join(model_path, file))  
+        
+        if contains_aenc: 
+            pass
+        else: 
+            components = [
+                "pData" if contains_pData else "",
+                "Intersection" if contains_intersection else "",
+                "Imputed" if contains_imputed else "",
+                "AutoEncoder" if contains_aenc else "",
+                "Scores" if contains_scores else ""
+            ]
 
-    # Iterate through all files in the folder
-    for file_name in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file_name)
+            # Join non-empty components into a single string with a separator (e.g., "_")
+            dataset = "_".join(filter(None, components))         
+            imps = load_feat_imp(model)
+            imps.loc[:, 'model_class'] =  model_name
+            imps.loc[:, 'dataset'] = dataset
+            imps_list.append(imps)
+        
+    df = pd.concat(imps_list, axis = 0)
+    return df
 
-        # Check if the file is a valid CSV file
-        if os.path.isfile(file_path) and file_name.endswith('.csv'):
-            try:
-                # Read the file into a DataFrame
-                df = pd.read_csv(file_path)
 
-                # Extract the 'mean_score' and 'std_score' from the first row
-                if 'mean_score' in df.columns and 'std_score' in df.columns:
-                    mean_score = df.loc[0, 'mean_score']
-                    std_score = df.loc[0, 'std_score']
+def load_model(model_path): 
+    """
+    Loads a trained model
+    
+    Args:
+        model_path (str): Path to the saved model file (pickle format).
+    
+    Returns:
+        object: The trained model instance.
+    """
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+    return model
 
-                    # Append the data to the list
-                    data.append({'Model': file_name, 'mean_score': mean_score, 'std_score': std_score})
-            except Exception as e:
-                print(f"Error reading file {file_name}: {e}")
 
-    # Create a DataFrame from the extracted data
-    result_df = pd.DataFrame(data)
+# def extract_scores_from_files(folder_path):
+#     """
+#     Reads all files in a given folder, extracts the 'mean_score' and 'std_score' 
+#     columns from the first row of each file, and saves them into a new DataFrame.
 
-    return result_df
+#     Args:
+#         folder_path (str): Path to the folder containing the files.
+
+#     Returns:
+#         pd.DataFrame: A DataFrame with columns ['Model', 'mean_score', 'std_score'].
+#     """
+#     # Initialize an empty list to store the extracted data
+#     data = []
+
+#     # Iterate through all files in the folder
+#     for file_name in os.listdir(folder_path):
+#         file_path = os.path.join(folder_path, file_name)
+
+#         # Check if the file is a valid CSV file
+#         if os.path.isfile(file_path) and file_name.endswith('.csv'):
+#             try:
+#                 # Read the file into a DataFrame
+#                 df = pd.read_csv(file_path)
+
+#                 # Extract the 'mean_score' and 'std_score' from the first row
+#                 if 'mean_score' in df.columns and 'std_score' in df.columns:
+#                     mean_score = df.loc[0, 'mean_score']
+#                     std_score = df.loc[0, 'std_score']
+
+#                     # Append the data to the list
+#                     data.append({'Model': file_name, 'mean_score': mean_score, 'std_score': std_score})
+#             except Exception as e:
+#                 print(f"Error reading file {file_name}: {e}")
+
+#     # Create a DataFrame from the extracted data
+#     result_df = pd.DataFrame(data)
+
+#     return result_df
 
